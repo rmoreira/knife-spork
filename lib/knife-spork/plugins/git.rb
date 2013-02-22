cce76daad92562146d0308eecf472fc19ccffc9b
Rafael Moreira <rafael.moreira@pearson.com>
2013-02-22 16:25:45 -0600

    testing promote in bupload

--- lib/knife-spork/plugins/git.rb
+++ lib/knife-spork/plugins/git.rb
@@ -118,7 +118,7 @@ module KnifeSpork
         ui.msg "Git Add #{filename}"
         if is_repo?(filepath)
           ui.msg "Git add'ing #{filepath}/#{filename}"
-          output = IO.popen("cd #{filepath};  git add #{filepath}/#{filename}")
+          output = IO.popen("cd #{filepath} && git rev-parse --show-toplevel && git add #{filepath}/#{filename}")
           Process.wait
           exit_code = $?
           ui.msg "Exit status: #{exit_code.exitstatus}"
